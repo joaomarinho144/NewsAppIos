b329142ef3b0f4693bd529d6aa11efc523d1334c
Joao <joaomarinho82@gmail.com>
2022-09-01 18:26:04 -0300

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Skyio Team on 01/09/22.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
